22ecc1cfdbe43e07f2ddb4afbaa5a85e0575d70e
Rakesh Upadhye <rakesh.upadhye@sonarsource,com>
2024-11-26 11:14:36 +0100

    First commit

---
+++ SourceProject/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceProject
+{
+    internal class Class1
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.ReadLine();
+        }
+
+    }
+}
